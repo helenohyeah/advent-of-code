f9df03c6319ed5ff7ea67f87bcc79c23947a5489
helenohyeah <ouyang.helen.com>
2020-12-08 10:03:16 -0500

    Create template for each day's problem

---
+++ 2020/_template/solution.js
@@ -0,0 +1,24 @@
+/* 
+===Part 1===
+
+*/
+
+const format = (input) => input.trim().split('\n');
+
+const partOne = (input) => {
+ 
+}
+
+/* 
+===Part 2===
+
+*/
+
+const partTwo = (input) => {
+
+}
+
+module.exports = {
+  partOne,
+  partTwo
+};
