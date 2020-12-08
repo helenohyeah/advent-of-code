3926b073bdb37ab3a0ffaa9f21d473c4798cda55
helenohyeah <ouyang.helen.com>
2020-12-08 10:03:53 -0500

    Create test.js template

---
+++ 2020/_template/test.js
@@ -0,0 +1,24 @@
+const assert = require('assert');
+const { partOne, partTwo } = require('./solution');
+
+describe('Day ', () => {
+  describe('Part One', () => {
+    it('should ...', () => {
+      const input = ``;
+    });
+  });
+  
+  describe('Part Two', () => {
+    it('should ...', () => {
+      const input = ``;
+    });
+  });
+  
+  describe('Solution', () => {
+    it('should print the solution given input', () => {
+      const input = require('./input.js');
+      console.log(`Part One: ${partOne(input)}`);
+      console.log(`Part Two: ${partTwo(input)}`);
+    });
+  });
+});
